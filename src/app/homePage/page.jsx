3ccee6b34978835b025ba7d70cfefe578c2e1577
omairZibew <omair@zibew.com>
2024-02-14 23:34:59 +0530

    COMMIT - CREATING STORE WHEN WE NEED

--- src/app/homePage/page.jsx
+++ src/app/homePage/page.jsx
@@ -1,12 +1,12 @@
 'use client'
 import ReduxProvider from "../provider";
-import AuthUpdater from "../../components/AuthUpdate";
+
 import AuthViewer from "../../components/AuthViewer";
 export default function Home() {
   return (
     <ReduxProvider>
       <main className="w-full h-screen grid grid-cols-2 place-items-center">
-        <AuthUpdater />
+        
         <AuthViewer />
       </main>
     </ReduxProvider>
--- src/components/AuthViewer.js
+++ src/components/AuthViewer.js
@@ -1,13 +1,28 @@
 import React from "react";
-import { useAppSelector } from "../lib/store";
+import { useSelector, useDispatch } from "react-redux";
+import { setAuthState } from "../lib/reducers/authSlice";
 
-const AuthViewer = () => {
-  const authState = useAppSelector((state) => state.auth.authState);
+const AuthViewerUpdater = () => {
+  const authState = useSelector((state) => state.auth.authState);
+  const dispatch = useDispatch();
+
+  const handleLogin = () => {
+    dispatch(setAuthState(true));
+  };
+
+  const handleLogout = () => {
+    dispatch(setAuthState(false));
+  };
+
   return (
     <div className="flex gap border border-1 border-black p-20">
       You are now {authState ? "Logged In" : "Logged Out"}
+      <div>
+        <button onClick={handleLogin}>Log in</button>
+        <button onClick={handleLogout}>Log out</button>
+      </div>
     </div>
   );
 };
 
-export default AuthViewer;
+export default AuthViewerUpdater;
